b3bf25a48f70cc55bdc73567767a51fff734ca11
Roshan Kumar <roshaen09@gmail.com>
2020-12-02 18:24:37 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+response = requests.post(
+    'https://api.remove.bg/v1.0/removebg',
+    files={'image_file': open('E:\\PYTHON\\python projects\\image_background\\rishav.jpg', 'rb')},
+    data={'size': 'auto'},
+    headers={'X-Api-Key': 'j2D5GtLi4Grdj8QJLSxq8cBc'},
+)
+try:
+    f = open('removed.png', 'wb')
+    f.write(response.content)
+except Exception as e:
+    print("Error", e)
